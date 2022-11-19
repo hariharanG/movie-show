66890a2a7513a2ee68bf7d7e395c9ad3154ededd
Hari G SVS <hariharan.ganpathy@gmail.com>
2022-11-19 07:59:27 +0530

    Renmed properly

--- show-service/src/test/java/com/xyz/MovieShowApplicationTests.java
+++ show-service/src/test/java/com/xyz/MovieShowApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dailycodebuffer;
+package com.xyz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class MovieShowApplicationTests {
 
 	@Test
 	void contextLoads() {
